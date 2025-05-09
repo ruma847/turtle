f769ae7ea152d645e58cb62b47220b7d1f568d35
ruma847 <rumaambarish@gmail.com>
2025-05-09 21:52:57 +0530

    Update prog3.py

---
+++ prog3.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.Screen().bgcolor("Green")
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(90)
+
+board.forward( 100)
+board.left(90)
+
+board.forward( 100)
+board.left(90)
+
+board.forward( 100)
+board.left(90)
